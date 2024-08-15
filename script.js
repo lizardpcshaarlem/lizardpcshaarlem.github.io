fe6ad4d098e6a7dc6bb1c6e386b157917e183742
lizardpcshaarlem <kimizwart@gmail.com>
2024-08-15 13:22:44 +0200

    update

--- script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('load', () => {
-    const elements = document.querySelectorAll('.fade-in');
-    elements.forEach((element, index) => {
-        setTimeout(() => {
-            element.style.opacity = 1;
-        }, index * 300); // 300ms vertraging tussen elk element
-    });
-});
